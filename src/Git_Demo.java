cd34fe34e788a6204e676497c113aa8740bd5fb7
vtompe <vtompe@vtompe-lap20.aurustech.com>
2022-09-21 18:51:18 +0530

    Commiting test2 to Branchx

--- src/Git_Demo.java
+++ src/Git_Demo.java
@@ -1,10 +1,14 @@
 
 public class Git_Demo {
-
+   public void display()
+   {
+	   System.out.println("Git is done");
+   }
 	public static void main(String[] args) {
 		
 		
-		
+		Git_Demo d=new Git_Demo();
+		d.display();
 		System.out.println("Hello Git");
 
 	}
